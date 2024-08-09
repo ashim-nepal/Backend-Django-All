5f4f6eb5deb50d911b94ce454a602d436411a639
ashim-nepal <npashim015@gmail.com>
2024-08-09 14:56:16 +0545

    authentication handling of user working with web app

--- Django_Course_Prac/myproject/posts/views.py
+++ Django_Course_Prac/myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_lists(request):
@@ -9,4 +10,8 @@ def posts_lists(request):
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {"post": post})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
     
